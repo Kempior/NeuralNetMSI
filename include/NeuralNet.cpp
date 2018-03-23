76a6ec358982ab6176a5cfe7161fa682ffa0a4f8
ExplodingPotato <wojtek4p4@gmail.com>
2018-03-23 20:00:12 +0100

    NeuralNet class: added Learning History, added Ability to learn step by step (though it's not checked if enough iterations have passed since the last change), added Ability to specify maxLearningIterations, added finding the current learningIteration as well as current updatesWithoutChenges, changed the weights to work on a value instead of a pointer

--- include/NeuralNet.cpp
+++ include/NeuralNet.cpp
@@ -1,22 +1,39 @@
 #include "NeuralNet.hpp"
 #include <iostream>
 
-NeuralNet::NeuralNet(unsigned int inputs, unsigned int outputs) : inputsWithBiasSize(inputs + 1), outputSize(outputs) {
-	weights = new std::vector<std::vector<float>>(outputSize, std::vector<float>(inputsWithBiasSize));
+NeuralNet::NeuralNet(unsigned int inputs, unsigned int outputs, unsigned int maximumLearningIterations) : inputsWithBiasSize(inputs + 1), outputSize(outputs), maxLearningIterations(maximumLearningIterations) {
+	ResetInternals();
 }
 
-NeuralNet::~NeuralNet() {
-	delete weights;
+NeuralNet::~NeuralNet() {}
+
+
+void NeuralNet::ResetInternals() {
+	weights = std::vector<std::vector<float>>(outputSize, std::vector<float>(inputsWithBiasSize));
+	history.push_back(weights);
+	
+	updatesWithoutChanges = 0;
+	learningIteration = 0;
 }
 
 void NeuralNet::LoadWeights (std::vector<std::vector<float>> newWeights) {
-	delete weights;
-	weights = new std::vector<std::vector<float>>(newWeights);
+	ResetInternals();
+	weights = std::vector<std::vector<float>>(newWeights);
 }
 
-std::vector<std::vector<float>> NeuralNet::Weights() {
-	return *weights;
+std::vector<std::vector<float>> NeuralNet::Weights() const {
+	return weights;
 }
+
+
+std::vector<std::vector<std::vector<float>>> NeuralNet::History() const {
+	return history;
+}
+
+std::vector<std::vector<float>> NeuralNet::History(int elem) const {
+	return history[elem];
+}
+
 
 std::vector<float> NeuralNet::PredictBiased(std::vector<float> input) {
 	
@@ -28,14 +45,14 @@ std::vector<float> NeuralNet::PredictBiased(std::vector<float> input) {
 	int oSize = weights[0].size();
 	
 	// Checking if input vector size is not WRONG
-	if (input.size() != (*weights)[0].size()) {
+	if (input.size() != weights[0].size()) {
 		return std::vector<float>();
 	}
 	
 	// Calculating the output values
 	for(int i = 0; i < iSize; i++) {
 		for(int j = 0; j < oSize; j++) {
-			output[j] += input[i] * (*weights)[j][i];
+			output[j] += input[i] * weights[j][i];
 		}
 	}
 	
@@ -55,6 +72,51 @@ std::vector<float> NeuralNet::Predict(std::vector<float> input) {
 	return PredictBiased(input);
 }
 
+unsigned int NeuralNet::LearningIteration() const {
+	return learningIteration;
+}
+
+unsigned int NeuralNet::UpdatesWithoutChanges() const {
+	return updatesWithoutChanges;
+}
+
+
+void NeuralNet::LearnStep(std::vector<std::vector<float>> learningValues) {
+	
+	updatesWithoutChanges++;
+	unsigned int currentValue = learningIteration % learningValues.size();
+	std::vector<float> prediction = PredictBiased(learningValues[currentValue]);
+	
+	// Just for the homework, delet this
+	std::cout << "Processed point: " << learningValues[currentValue][0] << ", " << learningValues[currentValue][1] << ", (" << learningValues[currentValue][2] << ")" << std::endl;
+	std::cout << "Weights:" << std::endl << weights[0][0] << ' ' << weights[0][1] << ' ' << weights[0][2] << std::endl;
+	std::cout << weights[1][0] << ' ' << weights[1][1] << ' ' << weights[1][2] << std::endl;
+	std::cout << weights[2][0] << ' ' << weights[2][1] << ' ' << weights[2][2] << std::endl << "Predicted output: ";
+	// End of delet this
+	
+	for (unsigned int j = 0; j < prediction.size(); j++) {
+		
+		// Delet this too
+		std::cout << prediction[j] << "\t";
+		// End of delet this
+		
+		// Yes, i'm doing comparisons on floats. Shush. They were assigned manually so it should work.
+		if ((j == currentValue && prediction[j] != 1) || 
+			(j != currentValue && prediction[j] != -1)) {
+			for (unsigned int k = 0; k < weights[0].size(); k++) {
+				weights[j][k] -= learningValues[currentValue][k];
+				updatesWithoutChanges = 0;
+			}
+		}
+	}
+	
+	// Also delet this
+	std::cout << "\nIteration number: " << learningIteration << ", currentValue =  " << currentValue << ", updatesWithouthanges =  " << updatesWithoutChanges << std::endl << std::endl;
+	// End of delet this
+	
+	learningIteration++;
+}
+
 void NeuralNet::Learn(std::vector<std::vector<float>> learningValues) {
 	
 	// Adding the bias untis
@@ -62,41 +124,8 @@ void NeuralNet::Learn(std::vector<std::vector<float>> learningValues) {
 		learningValues[i].push_back(-1);
 	}
 	
-	// Flag for the main learning loop
-	unsigned int updatesWithoutChanges = 0;
-	
 	// Main learning loop
-	for (unsigned int i = 0; i < 1000 && updatesWithoutChanges < learningValues.size() + 1; i++) {
-		
-		updatesWithoutChanges++;
-		unsigned int currentValue = i % learningValues.size();
-		std::vector<float> prediction = PredictBiased(learningValues[currentValue]);
-		
-		// Just for the homework, delet this
-		std::cout << "Processed point: " << learningValues[currentValue][0] << ", " << learningValues[currentValue][1] << ", (" << learningValues[currentValue][2] << ")" << std::endl;
-		std::cout << "Weights:" << std::endl << (*weights)[0][0] << ' ' << (*weights)[0][1] << ' ' << (*weights)[0][2] << std::endl;
-		std::cout << (*weights)[1][0] << ' ' << (*weights)[1][1] << ' ' << (*weights)[1][2] << std::endl;
-		std::cout << (*weights)[2][0] << ' ' << (*weights)[2][1] << ' ' << (*weights)[2][2] << std::endl << "Predicted output: ";
-		// End of delet this
-		
-		for (unsigned int j = 0; j < prediction.size(); j++) {
-			
-			// Delet this too
-			std::cout << prediction[j] << "\t";
-			// End of delet this
-			
-			// Yes, i'm doing comparisons on floats. Shush.
-			if ((j == currentValue && prediction[j] != 1) || 
-				(j != currentValue && prediction[j] != -1)) {
-				for (unsigned int k = 0; k < weights[0].size(); k++) {
-					(*weights)[j][k] -= learningValues[currentValue][k];
-					updatesWithoutChanges = 0;
-				}
-			}
-		}
-		
-		// Also delet this
-		std::cout << "\nIteration number: " << i << ", currentValue =  " << currentValue << ", updatesWithouthanges =  " << updatesWithoutChanges << std::endl << std::endl;
-		// End of delet this
+	for (learningIteration = 0; learningIteration < maxLearningIterations && updatesWithoutChanges < learningValues.size() + 1;) {
+		LearnStep(learningValues);
 	}
 }
--- include/NeuralNet.hpp
+++ include/NeuralNet.hpp
@@ -5,20 +5,35 @@
 
 class NeuralNet
 {
-	std::vector<std::vector<float>> *weights;
+	std::vector<std::vector<float>> weights;
+	std::vector<std::vector<std::vector<float>>> history;
+	unsigned int learningIteration;
+	unsigned int updatesWithoutChanges;
+	
+	void ResetInternals();
+	
 public:
-	NeuralNet(unsigned int inputs, unsigned int outputs);
+	NeuralNet(unsigned int inputs, unsigned int outputs, unsigned int maximumLearningIterations = 100);
 	~NeuralNet();
 	
 	std::vector<float> PredictBiased(std::vector<float> input);
 	std::vector<float> Predict(std::vector<float> input);
+	
+	void LearnStep(std::vector<std::vector<float>> learningValues);
 	void Learn(std::vector<std::vector<float>> learningValues);
+	unsigned int LearningIteration() const;
+	unsigned int UpdatesWithoutChanges() const;
 	
 	void LoadWeights (std::vector<std::vector<float>> newWeights);
-	std::vector<std::vector<float>> Weights();
+	std::vector<std::vector<float>> Weights() const;
+	
+	std::vector<std::vector<std::vector<float>>> History() const;
+	std::vector<std::vector<float>> History(int elem) const;
 	
 	const unsigned int inputsWithBiasSize;
 	const unsigned int outputSize;
+	
+	unsigned int maxLearningIterations;
 };
 
 #endif // NEURALNET_HPP
